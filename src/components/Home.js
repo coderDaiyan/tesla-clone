e040599f7ee8662cf1d028fd4ab1f932336ddc63
coderDaiyan <daiyanabdallah@gmail.com>
2021-05-16 19:12:29 +0600

    ADD: Sections for the TESLA

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import CarInfo from '../assets/carInfo.json';
+import Section from './Section';
+
+function Home() {
+    return (
+        <Container>
+            {/* <Section /> */}
+            {CarInfo.map((car) => (
+                <Section car={car} key={car.id} />
+            ))}
+        </Container>
+    );
+}
+
+export default Home;
+
+const Container = styled.div`
+    height: 100vh;
+`;
---
+++ src/components/Section.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+import Arrow from '../assets/images/down-arrow.svg';
+
+function Section({ car }) {
+    const { title, description, image, leftBtnText, rightBtnText } = car;
+    return (
+        <Wrap
+            style={{
+                background: `url(${image})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <ItemText>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </ItemText>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>{leftBtnText}</LeftButton>
+                    {rightBtnText && <RightButton>Existing Inventory</RightButton>}
+                </ButtonGroup>
+                <DownArrow src={Arrow} />
+            </Buttons>
+        </Wrap>
+    );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`;
+
+const LeftButton = styled.div`
+    background: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    margin: 8px;
+    cursor: pointer;
+`;
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    color: black;
+    opacity: 0.65;
+`;
+
+const DownArrow = styled.img`
+    /* margin-top: 20px; */
+    height: 40px;
+    overflow-y: hidden;
+    animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
